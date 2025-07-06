d764bcede58bd7c3786229f340b2a73e7307ce42
Bappa Kundu <bappakn22@gmail.com>
2025-07-06 07:28:45 +0530

    aman has added new rg

--- module/main.tf
+++ module/main.tf
@@ -7,7 +7,27 @@ module "rgs" {
 
 module "rgs_2" {
     source = "../resource"
-    rg_name = "rg-mahadev"
+    rg_name = "rg-shambhu"
     location_name = "east us"
   
 }
+
+
+
+
+
+
+
+
+
+
+# module "rgs_2" {
+#     source = "../resource"
+# <<<<<<< HEAD
+#     rg_name = "rg-mahadev"
+# =======
+#     rg_name = "rg-shahsi"
+# >>>>>>> main
+#     location_name = "east us"
+  
+# }
