590094ee72dff1dbf8574187cf710e193d736fdf
Bappa Kundu <bappakn22@gmail.com>
2025-07-07 08:27:08 +0530

    Initial commit

---
+++ module/main.tf
@@ -0,0 +1,33 @@
+module "rgs" {
+    source = "../resource"
+    rg_name = "rg-bholenath"
+    location_name = "east us"
+  
+}
+
+module "rgs_2" {
+    source = "../resource"
+    rg_name = "rg-sonali"
+    location_name = "east us"
+  
+}
+
+
+
+
+
+
+
+
+
+
+# module "rgs_2" {
+#     source = "../resource"
+# <<<<<<< HEAD
+#     rg_name = "rg-mahadev"
+# =======
+#     rg_name = "rg-shahsi"
+# >>>>>>> main
+#     location_name = "east us"
+  
+# }
